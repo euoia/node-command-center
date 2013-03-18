b908987c14c4c1588adec18c7d93de5a3388113e
James Pickard <james.pickard+github@gmail.com>
2013-03-18 19:56:57 -0400

    Bind the socket events based on an socketEvents object which can be augmented by client code.

--- index.js
+++ index.js
@@ -7,23 +7,12 @@ var sio = require('socket.io'),
 //         This is used for rejoining when the client sends a checkSession request.
 
 Chat = function(server, sessionStore, cookieParser) {
-	this.io = sio.listen(server);
-	this.sessionSockets = new SessionSockets(this.io, sessionStore, cookieParser),
-
-	this.setupListeners();
-}
-
-Chat.prototype.setupListeners = function() {
 	var $this = this;
 
-	this.sessionSockets.on('connection', function(err, socket, session) {
-		if (err) {
-			throw err;
-		}
-
-		socket.username = session.username;
-
-		socket.on('subscribe', function(data) {
+	// Array of socket events that can be handled.
+	// TODO: Reduce the LOC.
+	this.socketEvents  = {
+		'subscribe': function(socket, session, data) {
 			console.log('subscribe');
 			console.log(data);
 			if (data.roomName === undefined) {
@@ -76,9 +65,8 @@ Chat.prototype.setupListeners = function() {
 
 				$this.broadcastUserList(socket, data.roomName, _.uniq(usernamesInRoomAfterJoining));
 			}
-		});
-
-		socket.on('unsubscribe', function(data) {
+		},
+		'unsubscribe': function(socket, session, data) {
 			if (data.roomName === undefined) {
 				console.log(session.username + ' tried to unsubscribe but did not specify a roomName so the request is being discarded');
 				console.log(data);
@@ -94,11 +82,10 @@ Chat.prototype.setupListeners = function() {
 				roomName: data.roomName,
 				message: session.username + ' has unsubscribed ' + data.roomName + '.'
 			});
-		});
-
-		// Note that this event is fired before the socket is removed from the room
-		// list.
-		socket.on('disconnect', function() {
+		},
+		'disconnect': function(socket, session) {
+			// Note that this event is fired before the socket is removed from the room
+			// list.
 			console.log(session.username + ' disconnected.');
 
 			// TODO: Is there a way to get rooms without accessing a private member (socket.manager)?
@@ -132,10 +119,9 @@ Chat.prototype.setupListeners = function() {
 					console.log($this);
 					$this.broadcastUserList(socket, room, _.uniq(usernamesInRoomAfterLeaving));
 				}
-			}
-		});
-
-		socket.on('message', function(data) {
+			};
+		},
+		'message': function(socket, session, data) {
 			console.log('message');
 			console.log(data);
 
@@ -161,9 +147,8 @@ Chat.prototype.setupListeners = function() {
 				roomName: data.roomName,
 				message: data.message
 			});
-		});
-
-		socket.on('userList', function(data) {
+		},
+		'userList': function(socket, session, data) {
 			if (data.roomName === undefined) {
 				console.log(session.username + ' requested userList but did not specify a room name so it is being discarded.');
 				console.log(data);
@@ -175,8 +160,30 @@ Chat.prototype.setupListeners = function() {
 			console.log(users);
 
 			$this.emitUserList(socket, data.roomName, users);
-		});
+		}
+	};
 
+	this.io = sio.listen(server);
+	this.sessionSockets = new SessionSockets(this.io, sessionStore, cookieParser),
+	this.setupListeners();
+}
+
+Chat.prototype.setupListeners = function() {
+	var $this = this;
+
+	this.sessionSockets.on('connection', function(err, socket, session) {
+	console.log('socket connection');
+		if (err) {
+			throw err;
+		}
+
+		// Bind events.
+		for (event in $this.socketEvents) {
+			console.log('Bound ' + event);
+			socket.on(event, $this.socketEvents[event].bind(this, socket, session));
+		}
+
+		socket.username = session.username;
 	});
 };
 
@@ -192,6 +199,12 @@ Chat.prototype.broadcastUserList = function(socket, roomName, userList) {
 		roomName: roomName,
 		users: userList
 	});
+};
+
+// Allow the client code to add additional socket events.
+Chat.prototype.addSocketEvent = function (event, fn) {
+	console.log ('Adding socket event: ' + event);
+	this.socketEvents[event] = fn;
 };
 
 // ----------------------
