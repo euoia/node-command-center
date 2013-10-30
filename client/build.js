4d8422d6f36a9e16185620b42cb905996a7c91b2
James Pickard <james.pickard+github@gmail.com>
2013-10-30 16:18:11 +0000

    removing unneeded example files (index.ejs, layout.less) and renaming from chat.js to command-center-client.js

--- client/build.js
+++ client/build.js
@@ -1,10 +1,10 @@
 {
-    baseUrl: ".",
-    paths: {
-				jquery: "components/jquery/jquery",
-        underscore: "components/underscore/underscore",
-        "socket.io": "components/socket.io-client/dist/socket.io"
-    },
-    name: "chat",
-    out: "chat-built.js"
+  "baseUrl": ".",
+  "paths": {
+    "jquery": "components/jquery/jquery",
+    "underscore": "components/underscore/underscore",
+    "socket.io": "components/socket.io-client/dist/socket.io"
+  },
+  "name": "command-center-client",
+  "out": "command-center-client-built.js"
 }
