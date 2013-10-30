8d8e5ac3f915df6037e9e24983c499f9852052db
James Pickard <james.pickard+github@gmail.com>
2013-10-30 11:18:32 +0000

    re-indent

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var sio = require('socket.io'),
-	SessionSockets = require('session.socket.io'),
-	_ = require('underscore'),
-	util = require('util'),
-	sanitize = require('validator').sanitize;
+  SessionSockets = require('session.socket.io'),
+  _ = require('underscore'),
+  util = require('util'),
+  sanitize = require('validator').sanitize;
 
 // Adds the following keys to the session:
 // rooms - An array of room names that the session is currently present in.
@@ -13,146 +13,146 @@ var sio = require('socket.io'),
 //
 
 Chat = function(server, sessionStore, cookieParser) {
-	var $this = this;
+  var $this = this;
 
-	// Array of socket events that can be handled.
-	this.socketEvents  = {
-		'subscribe': function(socket, session, data) {
-			if (data.roomName === undefined) {
-				console.log('%s sent subscribe but did not specify a roomName.', session.username);
-				return;
-			}
+  // Array of socket events that can be handled.
+  this.socketEvents  = {
+    'subscribe': function(socket, session, data) {
+      if (data.roomName === undefined) {
+        console.log('%s sent subscribe but did not specify a roomName.', session.username);
+        return;
+      }
 
-			var usernamesInRoomBeforeJoining = _.pluck($this.io.sockets.clients(data.roomName), 'username');
+      var usernamesInRoomBeforeJoining = _.pluck($this.io.sockets.clients(data.roomName), 'username');
 
-			// Add the socket to the room.
-			socket.join(data.roomName);
+      // Add the socket to the room.
+      socket.join(data.roomName);
 
-			// Add the room name to the session.
-			if (_.contains(session.rooms, data.roomName) === false) {
-				session.rooms.push(data.roomName);
-				session.save();
-			}
+      // Add the room name to the session.
+      if (_.contains(session.rooms, data.roomName) === false) {
+        session.rooms.push(data.roomName);
+        session.save();
+      }
 
-			var usernamesInRoomAfterJoining = _.pluck($this.io.sockets.clients(data.roomName), 'username');
+      var usernamesInRoomAfterJoining = _.pluck($this.io.sockets.clients(data.roomName), 'username');
 
-			// Send the user list to the socket.
-			// A user may have multiple socket connections so we need the unique list of usernames.
-			$this.sendUserList(socket, data.roomName);
+      // Send the user list to the socket.
+      // A user may have multiple socket connections so we need the unique list of usernames.
+      $this.sendUserList(socket, data.roomName);
 
-			// If already present, the socket needs to join, but do not notify the room.
-			if (_.contains(usernamesInRoomBeforeJoining, socket.username)) {
-				$this.sendNotification(socket, util.format('You have rejoined %s.', data.roomName), data.roomName);
-			} else {
-				$this.sendNotification(socket, util.format('You have joined %s.', data.roomName), data.roomName);
-				$this.sendRoomNotification(socket, data.roomName, util.format('%s has joined %s.', session.username, data.roomName));
-				$this.broadcastUserList(socket, data.roomName, _.uniq(usernamesInRoomAfterJoining));
-			}
+      // If already present, the socket needs to join, but do not notify the room.
+      if (_.contains(usernamesInRoomBeforeJoining, socket.username)) {
+        $this.sendNotification(socket, util.format('You have rejoined %s.', data.roomName), data.roomName);
+      } else {
+        $this.sendNotification(socket, util.format('You have joined %s.', data.roomName), data.roomName);
+        $this.sendRoomNotification(socket, data.roomName, util.format('%s has joined %s.', session.username, data.roomName));
+        $this.broadcastUserList(socket, data.roomName, _.uniq(usernamesInRoomAfterJoining));
+      }
 
-			console.log('%s subscribed to %s.', session.usernane, data.roomName);
-		},
-		'unsubscribe': function(socket, session, data) {
-			if (data.roomName === undefined) {
-				console.log(util.format('%s tried to unsubscribe but did not specify a roomName so the request is being discarded', session.username));
-				return;
-			}
+      console.log('%s subscribed to %s.', session.usernane, data.roomName);
+    },
+    'unsubscribe': function(socket, session, data) {
+      if (data.roomName === undefined) {
+        console.log(util.format('%s tried to unsubscribe but did not specify a roomName so the request is being discarded', session.username));
+        return;
+      }
 
-			socket.leave(data.roomName);
-			$this.sendRoomNotification(socket, data.roomName, util.format('%s has left %s.', session.username, data.roomName));
+      socket.leave(data.roomName);
+      $this.sendRoomNotification(socket, data.roomName, util.format('%s has left %s.', session.username, data.roomName));
 
-			console.log(util.format('%s unsubscribed from %s.', session.username, data.roomName));
-		},
-		'disconnect': function(socket, session) {
-			// This event is fired BEFORE the socket is removed from the room list.
+      console.log(util.format('%s unsubscribed from %s.', session.username, data.roomName));
+    },
+    'disconnect': function(socket, session) {
+      // This event is fired BEFORE the socket is removed from the room list.
 
-			// TODO: Is there a way to get rooms without accessing a private member (socket.manager)?
-			var room = null,
-				socketRoomName = null,
-				timesInRoom = 0,
-				rooms = socket.manager.roomClients[socket.id], // Rooms associated with this socket.
-				usernamesInRoom = [],
-				usernamesInRoomAfterLeaving = [];
+      // TODO: Is there a way to get rooms without accessing a private member (socket.manager)?
+      var room = null,
+        socketRoomName = null,
+        timesInRoom = 0,
+        rooms = socket.manager.roomClients[socket.id], // Rooms associated with this socket.
+        usernamesInRoom = [],
+        usernamesInRoomAfterLeaving = [];
 
-			// For all rooms associated with the socket check if it is the last
-			// socket for that room for this user, and if so issue a message to the room.
-			for (socketRoomName in rooms) {
-				// Strip the socket.io leading '/'.
-				room = socketRoomName.substr(1);
+      // For all rooms associated with the socket check if it is the last
+      // socket for that room for this user, and if so issue a message to the room.
+      for (socketRoomName in rooms) {
+        // Strip the socket.io leading '/'.
+        room = socketRoomName.substr(1);
 
-				// Determine whether this is the last socket connection this user has to this room.
-				usernamesInRoom                = _.pluck($this.io.sockets.clients(room), 'username');
-				usernamesInRoomAfterLeaving    = _.without(usernamesInRoom, socket.username);
-				timesInRoom                    = usernamesInRoom.length - usernamesInRoomAfterLeaving.length;
+        // Determine whether this is the last socket connection this user has to this room.
+        usernamesInRoom                = _.pluck($this.io.sockets.clients(room), 'username');
+        usernamesInRoomAfterLeaving    = _.without(usernamesInRoom, socket.username);
+        timesInRoom                    = usernamesInRoom.length - usernamesInRoomAfterLeaving.length;
 
-				// Only display the disconnect message if this is the last socket the user has open to the room.
-				if (timesInRoom === 1) {
-					$this.sendRoomNotification(socket, room, util.format('%s has disconnected from %s.', session.username, room));
-					$this.broadcastUserList(socket, room, _.uniq(usernamesInRoomAfterLeaving));
-				}
-			}
+        // Only display the disconnect message if this is the last socket the user has open to the room.
+        if (timesInRoom === 1) {
+          $this.sendRoomNotification(socket, room, util.format('%s has disconnected from %s.', session.username, room));
+          $this.broadcastUserList(socket, room, _.uniq(usernamesInRoomAfterLeaving));
+        }
+      }
 
-			console.log('%s disconnected.', session.username);
-		},
-		'message': function(socket, session, data) {
-			// TODO: This kind of checking and logging is probably OTT. We could have some kind of separate validation module.
-			if (data.roomName === undefined) {
-				console.log('%s sent a message but did not specify roomName so it is being discarded.', session.username);
-				return;
-			}
+      console.log('%s disconnected.', session.username);
+    },
+    'message': function(socket, session, data) {
+      // TODO: This kind of checking and logging is probably OTT. We could have some kind of separate validation module.
+      if (data.roomName === undefined) {
+        console.log('%s sent a message but did not specify roomName so it is being discarded.', session.username);
+        return;
+      }
 
-			if (data.message === undefined) {
-				console.log('%s sent a message but did not specify message so it is being discarded.', session.username);
-				return;
-			}
+      if (data.message === undefined) {
+        console.log('%s sent a message but did not specify message so it is being discarded.', session.username);
+        return;
+      }
 
-			console.log('%s sent the following message to %s: %s', session.username, data.roomName, data.message);
-			$this.sendRoomMessage(socket, data.roomName, session.username, data.message);
-		},
-		'userList': function(socket, session, data) {
-			if (data.roomName === undefined) {
-				console.log('%s requested userList but did not specify roomName so it is being discarded.', session.username);
-				return;
-			}
+      console.log('%s sent the following message to %s: %s', session.username, data.roomName, data.message);
+      $this.sendRoomMessage(socket, data.roomName, session.username, data.message);
+    },
+    'userList': function(socket, session, data) {
+      if (data.roomName === undefined) {
+        console.log('%s requested userList but did not specify roomName so it is being discarded.', session.username);
+        return;
+      }
 
-			console.log('%s requested userList.', session.username);
-			$this.sendUserList(socket, data.roomName);
-		}
-	};
+      console.log('%s requested userList.', session.username);
+      $this.sendUserList(socket, data.roomName);
+    }
+  };
 
-	// Array of {ns: ns, event: event, fn: fn}
-	this.namespacedSocketEvents = [];
+  // Array of {ns: ns, event: event, fn: fn}
+  this.namespacedSocketEvents = [];
 
 
-	this.io = sio.listen(server);
-	this.sessionSockets = new SessionSockets(this.io, sessionStore, cookieParser);
-	this.setupListeners();
+  this.io = sio.listen(server);
+  this.sessionSockets = new SessionSockets(this.io, sessionStore, cookieParser);
+  this.setupListeners();
 };
 
 Chat.prototype.setupListeners = function() {
-	var $this = this;
+  var $this = this;
 
-	this.sessionSockets.on('connection', function(err, socket, session) {
-		console.log('Chat socket connection.');
-		if (err) {
-			throw(err);
-		}
+  this.sessionSockets.on('connection', function(err, socket, session) {
+    console.log('Chat socket connection.');
+    if (err) {
+      throw(err);
+    }
 
-		// Bind event handlers to the socket.
-		for (event in $this.socketEvents) {
-			console.log('Bound Chat event to socket: %s.', event);
-			socket.on(event, $this.socketEvents[event].bind(this, socket, session));
-		}
+    // Bind event handlers to the socket.
+    for (event in $this.socketEvents) {
+      console.log('Bound Chat event to socket: %s.', event);
+      socket.on(event, $this.socketEvents[event].bind(this, socket, session));
+    }
 
-		// Store anything extra on the socket object.
-		socket.username = session.username;
-	});
+    // Store anything extra on the socket object.
+    socket.username = session.username;
+  });
 };
 
 // Allow the client code to add namespaced socket events.
 Chat.prototype.addNamespacedListener = function (ns, event, fn) {
-	ns = '/' + ns;
-	console.log('Adding namespaced listener ns=%s event=%s.', ns, event);
-	this.sessionSockets.of(ns).on(event, fn);
+  ns = '/' + ns;
+  console.log('Adding namespaced listener ns=%s event=%s.', ns, event);
+  this.sessionSockets.of(ns).on(event, fn);
 };
 
 
@@ -163,32 +163,32 @@ Chat.prototype.addNamespacedListener = function (ns, event, fn) {
 
 // Send the user list of a given room to the socket.
 Chat.prototype.sendUserList = function(socket, roomName) {
-	var users = _.pluck(this.io.sockets.clients(roomName), 'username');
-	users = _.uniq(users);
+  var users = _.pluck(this.io.sockets.clients(roomName), 'username');
+  users = _.uniq(users);
 
-	socket.emit('userList', {
-		roomName: roomName,
-		users: users
-	});
+  socket.emit('userList', {
+    roomName: roomName,
+    users: users
+  });
 };
 
 // Send the user list of a given room to the socket.
 Chat.prototype.broadcastUserList = function(socket, roomName, userList) {
-	socket.broadcast.to(roomName).emit('userList', {
-		roomName: roomName,
-		users: userList
-	});
+  socket.broadcast.to(roomName).emit('userList', {
+    roomName: roomName,
+    users: userList
+  });
 };
 
 // Send a message from a user to a room; exluding a single socket.
 Chat.prototype.sendRoomMessage = function(socket, roomName, usernameFrom, message) {
-	message = sanitize(message).entityEncode();
-		socket.broadcast.to(roomName).emit('message', {
-			time: Date.now(),
-			username: usernameFrom,
-			roomName: roomName,
-			message: message
-		});
+  message = sanitize(message).entityEncode();
+    socket.broadcast.to(roomName).emit('message', {
+      time: Date.now(),
+      username: usernameFrom,
+      roomName: roomName,
+      message: message
+    });
 };
 
 // Send a direct message from a user to a socket.
@@ -196,23 +196,23 @@ Chat.prototype.sendRoomMessage = function(socket, roomName, usernameFrom, messag
 // roomName is optional - if not specified then the message will appear in all
 // room windows.
 Chat.prototype.sendMessage = function(socket, usernameFrom, message, roomName) {
-	message = sanitize(message).entityEncode();
-	socket.emit('message', {
-		time: Date.now(),
-		username: usernameFrom,
-		message: message,
-		roomName: roomName
-	});
+  message = sanitize(message).entityEncode();
+  socket.emit('message', {
+    time: Date.now(),
+    username: usernameFrom,
+    message: message,
+    roomName: roomName
+  });
 };
 
 // Send a notification to a room; exluding a single socket.
 Chat.prototype.sendRoomNotification = function(socket, roomName, message) {
-	message = sanitize(message).entityEncode();
-	socket.broadcast.to(roomName).emit('notification', {
-		time: Date.now(),
-		roomName: roomName,
-		message: message
-	});
+  message = sanitize(message).entityEncode();
+  socket.broadcast.to(roomName).emit('notification', {
+    time: Date.now(),
+    roomName: roomName,
+    message: message
+  });
 };
 
 // Send a notification to a socket in a room in a given room.
@@ -220,18 +220,18 @@ Chat.prototype.sendRoomNotification = function(socket, roomName, message) {
 // roomName is optional - if not specified then the message will appear in all
 // room windows.
 Chat.prototype.sendNotification = function(socket, message, roomName) {
-	message = sanitize(message).entityEncode();
-	socket.emit('notification', {
-		time: Date.now(),
-		message: message,
-		roomName: roomName
-	});
+  message = sanitize(message).entityEncode();
+  socket.emit('notification', {
+    time: Date.now(),
+    message: message,
+    roomName: roomName
+  });
 };
 
 // Allow the client code to add additional socket events.
 Chat.prototype.addListener = function (event, fn) {
-	console.log ('Adding listener: ' + event);
-	this.socketEvents[event] = fn;
+  console.log ('Adding listener: ' + event);
+  this.socketEvents[event] = fn;
 };
 
 // ----------------------
@@ -241,8 +241,8 @@ Chat.prototype.addListener = function (event, fn) {
 // Chat.initSession.
 // Initialize the express session object.
 Chat.initSession = function(session) {
-	console.log('initSession',session);
-	session.rooms = [];
+  console.log('initSession',session);
+  session.rooms = [];
 };
 
 module.exports = Chat;
