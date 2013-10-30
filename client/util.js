8594f2b9f30de49daedb85a6d62af57870992d31
James Pickard <james.pickard+github@gmail.com>
2013-10-30 16:29:05 +0000

    retab util.js

--- client/util.js
+++ client/util.js
@@ -1,34 +1,34 @@
 define(function() {
-	function Util () {
-	}
+  function Util () {
+  }
 
-	Util.formatDate = function(dateStr) {
-		var d = new Date(dateStr);
+  Util.formatDate = function(dateStr) {
+    var d = new Date(dateStr);
 
-		return this.formatNumberLength(d.getHours(), 2) +
-			":" + this.formatNumberLength(d.getMinutes(), 2);
-	};
+    return this.formatNumberLength(d.getHours(), 2) +
+      ":" + this.formatNumberLength(d.getMinutes(), 2);
+  };
 
-	Util.formatNumberLength = function(num, length) {
-		var r = "" + num;
-		while (r.length < length) {
-			r = "0" + r;
-		}
+  Util.formatNumberLength = function(num, length) {
+    var r = "" + num;
+    while (r.length < length) {
+      r = "0" + r;
+    }
 
-		return r;
-	};
+    return r;
+  };
 
-	Util.scrollDown = function(selector) {
-		// Scroll the selector to the bottom.
-		selector.animate({
-			scrollTop: 9999
-		}, 400);
+  Util.scrollDown = function(selector) {
+    // Scroll the selector to the bottom.
+    selector.animate({
+      scrollTop: 9999
+    }, 400);
 
-		// Clear the animation otherwise the user cannot scroll back up.
-		setTimeout(function clearAnimate() {
-			selector.animate({}, 1);
-		});
-	};
+    // Clear the animation otherwise the user cannot scroll back up.
+    setTimeout(function clearAnimate() {
+      selector.animate({}, 1);
+    });
+  };
 
-	return Util;
+  return Util;
 });
